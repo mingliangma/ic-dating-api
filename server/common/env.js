4adcfd7602b911f903652f1da5748bad1d6fb790
Mingliang Ma <mingliang.ma@gmail.com>
2017-10-02 15:27:32 -0400

    Revert .env.test

--- server/common/env.js
+++ server/common/env.js
@@ -1,9 +1,3 @@
 import dotenv from 'dotenv';
-
-if (process.env.NODE_ENV === 'test') {
-  dotenv.config({ path: '.env.test' });
-  dotenv.config();
-} else {
-  dotenv.config();
-}
+dotenv.config();
 
